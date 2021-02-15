391cdf9485e012c225a0e11c52d887d850200f9b
Kai Jellinghaus <contact@kaij.tech>
2021-02-15 10:33:42 +0100

    Optimize Backtracking Map Generator

--- src/de/kaij_noah/it/textadventure/mapgen/BacktrackingMapGenerator.java
+++ src/de/kaij_noah/it/textadventure/mapgen/BacktrackingMapGenerator.java
@@ -15,7 +15,6 @@ public final class BacktrackingMapGenerator implements IMapGenerator
     public TileTemplate[][][] Generate(int sizeX, int sizeY, int sizeZ)
     {
         var tiles = new TileTemplate[sizeX][sizeY][sizeZ];
-        var visited = new ArrayList<Integer>();
         var random = new Random();
 
         var stack = new Stack<Integer>();
@@ -24,8 +23,6 @@ public final class BacktrackingMapGenerator implements IMapGenerator
         int pathI = 0;
         while (true)
         {
-            visited.add(current);
-
             var x = Util.GetX(current, sizeX, sizeY, sizeZ);
             var y = Util.GetY(current, sizeX, sizeY, sizeZ);
             var z = Util.GetZ(current, sizeX, sizeY, sizeZ);
@@ -43,47 +40,41 @@ public final class BacktrackingMapGenerator implements IMapGenerator
 
             if (x - 1 >= 0)
             {
-                var t = Util.Index2D(x - 1, y, z, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x - 1][y][z] == null)
                     for (int i = 0; i < sameLevelMultiplier; i++)
                         possibleDirs.add(new Vector3I(x - 1, y, z));
             }
 
             if (y - 1 >= 0)
             {
-                var t = Util.Index2D(x, y - 1, z, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x][y - 1][z] == null)
                     for (int i = 0; i < sameLevelMultiplier; i++)
                         possibleDirs.add(new Vector3I(x, y - 1, z));
             }
 
             if (z - 1 >= 0)
             {
-                var t = Util.Index2D(x, y, z - 1, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x][y][z - 1] == null)
                     possibleDirs.add(new Vector3I(x, y, z - 1));
             }
 
             if (x + 1 < sizeX)
             {
-                var t = Util.Index2D(x + 1, y, z, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x + 1][y][z] == null)
                     for (int i = 0; i < sameLevelMultiplier; i++)
                         possibleDirs.add(new Vector3I(x + 1, y, z));
             }
 
             if (y + 1 < sizeY)
             {
-                var t = Util.Index2D(x, y + 1, z, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x][y + 1][z] == null)
                     for (int i = 0; i < sameLevelMultiplier; i++)
                         possibleDirs.add(new Vector3I(x, y + 1, z));
             }
 
             if (z + 1 < sizeZ)
             {
-                var t = Util.Index2D(x, y, z + 1, sizeX, sizeY, sizeZ);
-                if (!visited.contains(t))
+                if (tiles[x][y][z + 1] == null)
                     possibleDirs.add(new Vector3I(x, y, z + 1));
             }
 
