c5ac33c6fd2fef45cbfbca452bdbbc431a0da702
Kai Jellinghaus <contact@kaij.tech>
2021-02-16 17:12:40 +0100

    Add Time to Debug Renderer

--- src/de/kaij_noah/it/textadventure/renderers/DebugRenderer.java
+++ src/de/kaij_noah/it/textadventure/renderers/DebugRenderer.java
@@ -7,14 +7,19 @@ import de.kaij_noah.it.textadventure.base.IRenderer;
 public final class DebugRenderer implements IRenderer
 {
     private int frameCount = 0;
+    private long lastFrame;
 
     @Override
     public void Render(IConsole console, GameState gameState)
     {
+        var currentTime = System.currentTimeMillis();
+        var diff = currentTime - lastFrame;
+        lastFrame = currentTime;
         var p = gameState.getPlayerEntity().getPosition();
         var x = p.X;
         var y = p.Y;
         var z = p.Z;
-        console.write(String.format("Frame: %s | Pos: %s %s %s\n", frameCount++, x, y, z));
+        console.write(String.format("Frame: %s | Time: %sms | Pos: %s %s %s\n", frameCount++, diff, x, y, z));
+        console.newLine();
     }
 }
